347c3743144793adb2a0289fc78bb084e2fee622
renebohne <rene.bohne@gmail.com>
2022-01-28 21:04:20 +0100

    initial commit

---
+++ RoboHeart.cpp
@@ -0,0 +1,39 @@
+/*!
+ *  @file GEENYmodem.cpp
+ *
+ *  @mainpage Arduino library for the RoboHeart Hardware.
+ *
+ *  @section intro_sec Introduction
+ *
+ * 	Arduino library for the RoboHeart.
+ *
+ *  @section dependencies Dependencies
+ *  This library depends on the tinyGSM library, the HTTPClient library, and the PubSub library.
+ 
+ *
+ *  @section author Author
+ *
+ *  Augmented Robotics
+ *
+ * 	@section license License
+ *
+ * 	MIT (see license.txt)
+ */
+
+#include "Arduino.h"
+#include "RoboHeart.h"
+
+RoboHeart::RoboHeart(void)
+{
+
+}
+
+RoboHeart::~RoboHeart(void)
+{
+
+}
+
+bool GEENYmodem::begin()
+{
+    return true;
+}
---
+++ RoboHeart.h
@@ -0,0 +1,19 @@
+/*!
+ *  @file RoboHeart.h
+ *
+ *
+ *	MIT license (see license.txt)
+ */
+#ifndef _ROBOHEART_H
+#define _ROBOHEART_H
+
+class RoboHeart
+{
+    public:
+        RoboHeart();
+        ~RoboHeart();
+
+        bool begin();
+}
+
+#endif
